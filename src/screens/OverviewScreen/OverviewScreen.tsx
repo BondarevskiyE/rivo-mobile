5147a545c1c97f2d371d6fd330d76d30294db508
Egor Bondarevskii <egorbondarevskii99@gmail.com>
2024-07-02 20:42:31 +1000

    add overview card animation

--- src/screens/OverviewScreen/OverviewScreen.tsx
+++ src/screens/OverviewScreen/OverviewScreen.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
-import {StyleSheet, View, ScrollView, Dimensions} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from 'react-native';
+import ReAnimated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 import {Header} from './components';
 import {CardWallet} from '@/components';
@@ -9,20 +22,98 @@ import {CashAccount, StrategiesList} from './components';
 const {height} = Dimensions.get('window');
 
 export const OverviewScreen = () => {
+  const [isHideCard, setIsHideCard] = useState(false);
+
+  const cardAnimationValue = useSharedValue(1);
+
+  const onHandleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+
+    if (!!offsetY && !isHideCard) {
+      setIsHideCard(true);
+    }
+
+    if (offsetY === 0) {
+      setIsHideCard(false);
+    }
+  };
+
+  const cardScaleStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          cardAnimationValue.value,
+          [0, 1],
+          [0.2, 1],
+          Extrapolation.CLAMP,
+        ),
+      },
+      {
+        translateY: interpolate(
+          cardAnimationValue.value,
+          [0, 1],
+          [-30, 0],
+          Extrapolation.CLAMP,
+        ),
+      },
+    ],
+  }));
+
+  const cardOpacityStyle = useAnimatedStyle(() => ({
+    opacity: cardAnimationValue.value,
+  }));
+
+  useEffect(() => {
+    if (isHideCard) {
+      cardAnimationValue.value = withTiming(0, {duration: 200});
+      return;
+    }
+    cardAnimationValue.value = withTiming(1, {duration: 200});
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isHideCard]);
+
+  const cardHeightStyle = useAnimatedStyle(() => {
+    const interpolatedHeight = interpolate(
+      cardAnimationValue.value,
+      [0, 1],
+      [0, 25],
+      {extrapolateRight: Extrapolation.CLAMP},
+    );
+    const interpolatedMarginTop = interpolate(
+      cardAnimationValue.value,
+      [0, 1],
+      [0, 40],
+      {extrapolateRight: Extrapolation.CLAMP},
+    );
+    return {
+      height: `${interpolatedHeight}%`,
+      marginTop: interpolatedMarginTop,
+    };
+  });
+
   return (
     <>
-      <Header />
+      <Header cardAnimationValue={cardAnimationValue} />
 
-      <ScrollView style={styles.container}>
+      <ReAnimated.ScrollView
+        style={styles.container}
+        onScroll={onHandleScroll}
+        bounces={false}>
         <View style={styles.scroll}>
-          <View style={styles.cardWalletContainer}>
+          <ReAnimated.View
+            style={[
+              styles.cardWalletContainer,
+              cardScaleStyles,
+              cardOpacityStyle,
+              cardHeightStyle,
+            ]}>
             <CardWallet />
-          </View>
+          </ReAnimated.View>
           <OnboardingTasks />
           <CashAccount />
           <StrategiesList />
         </View>
-      </ScrollView>
+      </ReAnimated.ScrollView>
     </>
   );
 };
@@ -30,18 +121,18 @@ export const OverviewScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: Colors.ui_background,
     height,
     paddingHorizontal: 12,
-    // paddingBottom: 20,
   },
   cardWalletContainer: {
     width: '73%',
     alignSelf: 'center',
+    // marginBottom: 40,
+    // paddingTop: 20,
     marginVertical: 40,
+    transformOrigin: 'top',
   },
   scroll: {
-    // overflow: 'visible',
     paddingBottom: 100,
   },
 });
--- src/screens/OverviewScreen/components/Header/Header.tsx
+++ src/screens/OverviewScreen/components/Header/Header.tsx
@@ -1,12 +1,55 @@
 import {DEFAULT_USER_PHOTO} from '@/shared/constants';
 import {useUserStore} from '@/store/useUserStore';
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ImageBackground,
+  Text,
+} from 'react-native';
 import {PointsCounter} from '@/components/PointsCounter';
-import {Colors} from '@/shared/ui';
+import {Colors, Fonts, Images} from '@/shared/ui';
+import ReAnimated, {
+  Extrapolation,
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+import {useBalanceStore} from '@/store/useBalanceStore';
 
-export const Header = () => {
+interface Props {
+  cardAnimationValue: SharedValue<number>;
+}
+
+const {width} = Dimensions.get('window');
+
+export const Header: React.FC<Props> = ({cardAnimationValue}) => {
+  const userBalance = useBalanceStore(state => state.userBalance);
+
   const user = useUserStore(state => state.userInfo);
+
+  const cardAnimationStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: interpolate(
+          cardAnimationValue.value,
+          [1, 0],
+          [100, 0],
+          Extrapolation.CLAMP,
+        ),
+      },
+      {
+        scale: interpolate(
+          cardAnimationValue.value,
+          [1, 0],
+          [2.3, 1],
+          Extrapolation.CLAMP,
+        ),
+      },
+    ],
+  }));
 
   return (
     <View style={styles.container}>
@@ -14,6 +57,17 @@ export const Header = () => {
         source={{uri: user?.photo || DEFAULT_USER_PHOTO}}
         style={styles.photo}
       />
+      <ReAnimated.View style={[styles.card, cardAnimationStyles]}>
+        <ImageBackground
+          style={styles.cardImage}
+          source={Images.userCard}
+          resizeMode="contain">
+          <View style={styles.balanceContainer}>
+            <Text style={styles.dollar}>$</Text>
+            <Text style={styles.balance}>{userBalance}</Text>
+          </View>
+        </ImageBackground>
+      </ReAnimated.View>
       <PointsCounter />
     </View>
   );
@@ -21,17 +75,50 @@ export const Header = () => {
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     paddingTop: 7,
     paddingHorizontal: 16,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     zIndex: 2,
     borderStartColor: Colors.ui_beige_30,
+    overflow: 'hidden',
+  },
+  card: {
+    position: 'absolute',
+    left: width / 2 - 26,
+    top: 0,
+    width: 52,
+    height: 32,
+  },
+  cardImage: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
   photo: {
     width: 32,
     height: 32,
     borderRadius: 16,
+  },
+  dollar: {
+    fontFamily: Fonts.medium,
+    color: Colors.ui_orange_40,
+    fontSize: 12,
+    lineHeight: 13.2,
+  },
+  balanceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingLeft: 4,
+  },
+  balance: {
+    fontFamily: Fonts.medium,
+    color: Colors.ui_orange_45,
+    fontSize: 12,
+    lineHeight: 13.2,
   },
 });
