c43c70e1a24217e3992c345a93ad7fed3e460be2
Egor Bondarevskii <egorbondarevskii99@gmail.com>
2024-07-02 21:50:54 +1000

    onboarding tasks shining

--- src/components/onboarding/OnboardingTasks.tsx
+++ src/components/onboarding/OnboardingTasks.tsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 
 import {useUserStore} from '@/store/useUserStore';
 import {onboardingData} from '@/shared/config/onboardingData';
 import {Colors, Fonts} from '@/shared/ui';
 import {GradientText} from '../general/GradientText';
+import {CardShineIcon} from '@/shared/ui/icons';
+import ReAnimated, {
+  Extrapolation,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+
+const SHINE_INTERVAL_TIME = 5000;
 
 export const OnboardingTasks = () => {
   const onboardingStepNumber = useUserStore(state => state.onboardingStep);
+
+  const shineGradientValue = useSharedValue(0);
 
   const stepData = onboardingData[onboardingStepNumber - 1];
 
   const onboardingsTasksLength = onboardingData.length;
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      shineGradientValue.value = withSequence(
+        withTiming(1, {duration: 1700}),
+        withTiming(0, {duration: 0}),
+      );
+      // shineGradientValue.value = 1;
+    }, SHINE_INTERVAL_TIME);
+    return () => {
+      clearInterval(interval);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const shineGradientStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: interpolate(
+          shineGradientValue.value,
+          [0, 1],
+          [-230, 500],
+          Extrapolation.EXTEND,
+        ),
+      },
+    ],
+  }));
+
   return (
     <View style={styles.container}>
-      <ImageBackground source={stepData.image}>
+      <ImageBackground source={stepData.image} style={styles.backgroundImage}>
+        <ReAnimated.View
+          style={[styles.shineGradientContainer, shineGradientStyles]}>
+          <CardShineIcon />
+        </ReAnimated.View>
         <View style={styles.task}>
           <View>
             <Text
@@ -69,5 +113,16 @@ const styles = StyleSheet.create({
     fontFamily: Fonts.medium,
     fontSize: 16,
     lineHeight: 22.4,
+  },
+  backgroundImage: {
+    position: 'relative',
+  },
+  shineGradientContainer: {
+    position: 'absolute',
+    top: -40,
+    left: -230,
+    width: 40,
+    height: '100%',
+    zIndex: 3,
   },
 });
